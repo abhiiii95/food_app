c0f2a308a61a0dfff25fda16127890a4548279db
Codergirllll <himgan1611@gmail.com>
2024-09-03 22:26:33 +0530

    Bootstrap

--- src/app/resturant/page.js
+++ src/app/resturant/page.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
 
 const RasturantPage = () => {
   return (
     <div>
       <h1>Resturant logIn/ SignUp Page</h1>
+      <Button variant="success">Success</Button>{' '}
     </div>
   )
 }

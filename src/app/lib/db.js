7a7117c03b3752f93ca41b0fb9f54237a130cb1a
Himani Gangwar <himani.gangwar@w2c.co.in>
2024-09-23 10:48:34 +0530

    Solve DB Authentiction Problem

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,6 +1,9 @@
 
 
-const {username,password}= process.env
+const { User_name, Password } = process.env
 
-export const connectionURL = `mongodb+srv://${username}:${password}@cluster0.hiv0k.mongodb.net/Restro?retryWrites=true&w=majority&appName=Cluster0`
+// console.log(User_name, "Himani", Password);
+
+export const connectionURL = `mongodb+srv://${User_name}:${Password}@cluster0.hiv0k.mongodb.net/Restro?retryWrites=true&w=majority&appName=Cluster0`
+// export const connectionURL = "mongodb+srv://himgan1611:Himani1611@cluster0.hiv0k.mongodb.net/Restro?retryWrites=true&w=majority&appName=Cluster0"
 
